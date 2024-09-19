f4daf6a9ab67fbc766338b8a8abeaabcfe7315c6
Milan MAC Pro <phurbas@M-MacBook-Pro.local>
2024-09-19 11:45:36 +1000

    findtrend witg html,css and JS

---
+++ index.js
@@ -0,0 +1,29 @@
+const findtrendLogo = document.querySelector("#link-findtrend");
+findtrendLogo.innerText = "Findtrend";
+const linkAbout = document.querySelector("#link-about");
+linkAbout.innerText = "About";
+const linkHowItWorks = document.querySelector("#link-how-it-works");
+linkHowItWorks.innerText = "How it works";
+const linkPricing = document.querySelector("#link-pricing");
+linkPricing.innerText = "Pricing";
+const solution = document.querySelector("#link-solution");
+solution.innerText = "Solution";
+const features = document.querySelector("#link-features");
+features.innerText = "Features";
+
+const login = document.querySelector("#link-login");
+login.innerText = "Login";
+const register = document.querySelector("#link-register");
+register.innerText = "Register";
+
+const minimiseTabs = document.querySelector("#tabs-text");
+minimiseTabs.innerText = "Minimize your tabs.";
+const trendsText = document.querySelector("#trends-text");
+trendsText.innerText = "Find the trends!";
+const panelP = document.querySelector("#panel-text1");
+panelP.innerText =
+  "Bruce Lee said i don't fear the man who practiced 1000 kicks once!";
+const panelP2 = document.querySelector("#panel-text2");
+panelP2.innerText = "I fear the man who practiced one kick one thousand times.";
+const heroButton = document.querySelector("#hero-button");
+heroButton.innerText = "Let's Go 🐦‍🔥";
